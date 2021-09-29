a1e67b325906b15cab27ea8b56eeb4ebdd616887
rubenbr99 <91422840+rubenbr99@users.noreply.github.com>
2021-09-29 18:17:05 +0200

    pata

--- AsterixDecoder/ConsoleApp1/Program.cs
+++ AsterixDecoder/ConsoleApp1/Program.cs
@@ -18,22 +18,45 @@ namespace ConsoleApp1
             asterixFile.leer();
             List<CAT21> listaCAT21 = new List<CAT21>();
             listaCAT21 = asterixFile.getListCAT21();
-            int cont = 0;
-            int pack = 0;
-            while (pack < listaCAT21.Count  && pack<20)
-            {
-                cont = 0;
-                Console.WriteLine("Paquete "+pack+":");
-                while (cont < Convert.ToInt32(listaCAT21[pack].getCat(2), 16))
-                {
-                    Console.WriteLine(listaCAT21[pack].getCat(cont));
-                    cont++;
 
-                }
-                Console.WriteLine("Longitud " + Convert.ToString(cont));
-                pack++;
-            }
-            
+
+
+
+
+
+
+            //int cont = 0;
+            //int pack = 0;
+            //while (pack < listaCAT21.Count  && pack<20)
+            //{
+            //    cont = 0;
+            //    Console.WriteLine("Paquete "+pack+":");
+            //    while (cont < Convert.ToInt32(listaCAT21[pack].getCat(2), 16))
+            //    {
+            //        Console.WriteLine(listaCAT21[pack].getCat(cont));
+            //        cont++;
+
+
+            //    }
+            //    Console.WriteLine("Longitud " + Convert.ToString(cont));
+            //    pack++;
+            //}
+            //int cont = 0;
+            //int pack = 0;
+            //string mensaje = "";
+            //while (pack < listaCAT21.Count)
+            //{
+            //    cont = 0;
+
+            //    while (cont < Convert.ToInt32(listaCAT21[pack].getCat(2), 16))
+            //    {
+            //        mensaje+=listaCAT21[pack].getCat(cont);
+            //        cont++;
+            //    }
+            //    pack++;
+            //}
+            //Console.WriteLine(mensaje);
+
 
             //Console.WriteLine("Paquete 2:");
             //cont = 0;
